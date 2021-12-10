73da8130044eec58d9fa1e2971e28447ffa670f6
Jose Jovian <josejovian55@gmail.com>
2021-12-10 14:25:40 +0700

    Added a stylesheet link for FontAwesome Icons

--- components/meta.js
+++ components/meta.js
@@ -6,6 +6,7 @@ const Meta = ({ title, description }) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     </Head>
   );
 };
